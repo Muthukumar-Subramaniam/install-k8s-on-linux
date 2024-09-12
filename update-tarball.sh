920d361097cae1754b96eab0a0e6f4ac8fd8439b
Muthukumar Subramaniam <msmkumar.eee@gmail.com>
2024-09-12 10:58:42 +0530

    Update: inst-k8s-ansible.tar.gz for bug fix release v2.1.1

--- update-tarball.sh
+++ update-tarball.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+> ./inst-k8s-ansible/host-control-plane
+> ./inst-k8s-ansible/host-workers
+> ./inst-k8s-ansible/pod-network-cidr
+> ./inst-k8s-ansible/inventory
 > ./inst-k8s-ansible/logs-inst-k8s-ansible-play-output.txt
 tar -czvf ./inst-k8s-ansible.tar.gz ./inst-k8s-ansible
