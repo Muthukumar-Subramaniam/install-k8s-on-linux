c37961a5772e357aa7d1ca0e57f929ebb865b4da
Muthukumar Subramaniam <msmkumar.eee@gmail.com>
2024-09-15 03:16:36 +0530

    Bug fix : inst-k8s-ansible/setup.sh

--- inst-k8s-ansible/setup.sh
+++ inst-k8s-ansible/setup.sh
@@ -25,7 +25,7 @@ fn_print_fail () {
 }
 
 fn_msg_setup () {
-	fn_print_note "Run ./setup.sh again once fixed!\n"
+	fn_print_note "Run the ./setup.sh script again once the issue is fixed!\n"
 }
 
 fn_check_files () {
@@ -48,13 +48,13 @@ fn_check_files () {
 
 }
 	
-fn_print_msg "Check required files . . . "
+fn_print_msg "Check the required files . . . "
 fn_check_files "${var_host_file_cp}"
 fn_check_files "${var_host_file_w}"
 fn_check_files "${var_pod_network_file}"
 fn_print_success "[done]\n"
 
-fn_print_msg "Check single control plane host is provided . . ."
+fn_print_msg "Check if a single control plane host is provided . . ."
 if [[ "$(cat ${var_host_file_cp} | wc -l )" -ne 1 ]]
 then
 	fn_print_fail "\nFile ${var_host_file_cp} should only contain exactly one host entry! \n"
@@ -65,41 +65,41 @@ fn_print_success "[done]\n"
 
 var_pod_network_cidr=$(cat ${var_pod_network_file})
 
-fn_print_msg "Validate pod network CIDR . . . "
+fn_print_msg "Validate the pod network CIDR . . . "
 
 if [[ ! "${var_pod_network_cidr}" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.0/[0-9]{1,2}$ ]]
 then
-	fn_print_fail "\nInvalid pod network CIDR ${var_pod_network_cidr} provided in file ${var_pod_network_file} ! \n"
+	fn_print_fail "\nInvalid pod network CIDR ${var_pod_network_cidr} is provided in the file ${var_pod_network_file} ! \n"
 	fn_msg_setup
 	exit
 
 elif ! echo "${var_pod_network_cidr}" |  grep -E '^(192\.168|10\.|172\.1[6789]\.|172\.2[0-9]\.|172\.3[01]\.)' &>/dev/null
 then
-        fn_print_fail "\nPod network ${var_pod_network_cidr} provided in file ${var_pod_network_file} doesn't fall under private address space ( RFC 1918 ) ! \n"
+        fn_print_fail "\nThe pod network CIDR ${var_pod_network_cidr} provided in the file ${var_pod_network_file} doesn't fall under private address space ( RFC 1918 ) ! \n"
 	fn_msg_setup
 	exit
 
 elif  echo "${var_pod_network_cidr}" | grep '^10\.96\.' &>/dev/null
 then
-	fn_print_fail "\nPod network ${var_pod_network_cidr} provided in file ${var_pod_network_file} overlaps with k8s default internal Cluster IP network 10.96.0.0/16 ! \n"
+	fn_print_fail "\nThe pod network CIDR ${var_pod_network_cidr} provided in the file ${var_pod_network_file} overlaps with kubernetes default internal Cluster IP network 10.96.0.0/16 ! \n"
 	fn_msg_setup
 	exit
 
 elif [[ "${var_pod_network_cidr##*/}" -lt 16 ]] || [[ "${var_pod_network_cidr##*/}" -gt 28 ]]
 then
-        fn_print_fail "\nInvalid pod network CIDR prefix /${var_pod_network_cidr##*/} in file ${var_pod_network_file}, as a best practice only /16 to /28 is accepted! \n"
+        fn_print_fail "\nInvalid pod network CIDR prefix /${var_pod_network_cidr##*/} in the file ${var_pod_network_file}, as a best practice only /16 to /28 is accepted! \n"
 	fn_msg_setup
 	exit
 fi
 
 fn_print_success "[done]\n"
 
-fn_print_msg "Update pod network provided to vars.yaml  . . . "
+fn_print_msg "Update the pod network CIDR provided to vars.yaml  . . . "
 sed -i '/var_k8s_pod_network_cidr/d' ./vars.yaml
 echo "var_k8s_pod_network_cidr: \"${var_pod_network_cidr}\"" >> ./vars.yaml
 fn_print_success "[done]\n"
 
-fn_print_msg "Update hosts provided to inventory . . . "
+fn_print_msg "Update the hosts provided to the inventory . . . "
 cat >./inventory << EOF
 ##Version : v2.1.2
 local-ansible-control-host ansible_host=localhost ansible_connection=local
@@ -117,20 +117,20 @@ do
 	if [ ! -z "${var_ansible_user}" ];then break;fi
 done
 
-fn_print_msg "\nRun ansible ping test against host provided in ./host-control-plane . . . \n" 
+fn_print_msg "\nRun the ansible ping test against the host provided in ./host-control-plane . . . \n" 
 
 if ! ansible -u "${var_ansible_user}" -m ping k8s_cluster_ctrl_plane_node
 then 
-	fn_print_fail "\nThere are some issues while doing ansible ping test with control plane host, Please fix it.\n"
+	fn_print_fail "\nThere are some issues while doing the ansible ping test with the control plane host, Please fix it.\n"
 	fn_msg_setup
 	exit
 fi
 
-fn_print_msg "\nRun ansible ping test hosts provided in ./host-workers . . . \n" 
+fn_print_msg "\nRun the ansible ping test against the hosts provided in ./host-workers . . . \n" 
 
 if ! ansible -u "${var_ansible_user}" -m ping k8s_cluster_worker_nodes
 then 
-	fn_print_fail "\nThere are somes issue while doing ansible ping test with worker hosts, Please fix it.\n"
+	fn_print_fail "\nThere are somes issue while doing the ansible ping test with the worker hosts, Please fix it.\n"
 	fn_msg_setup
 	exit
 fi
