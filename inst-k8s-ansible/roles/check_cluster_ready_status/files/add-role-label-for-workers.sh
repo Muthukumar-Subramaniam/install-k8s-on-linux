f2ed17bc8d6445d257e849550e8cfa209516b485
Muthukumar Subramaniam <msmkumar.eee@gmail.com>
2024-09-16 16:29:14 +0530

    Bug fix: Moved inst-k8s-ansible/scripts/add-role-label-for-workers.sh to inst-k8s-ansible/roles/check_cluster_ready_status/files/add-role-label-for-workers.sh and updated inst-k8s-ansible/vars.yaml

--- inst-k8s-ansible/roles/check_cluster_ready_status/files/add-role-label-for-workers.sh
+++ inst-k8s-ansible/roles/check_cluster_ready_status/files/add-role-label-for-workers.sh
@@ -2,12 +2,7 @@
 var_worker_role_name=$1
 var_worker_role_name=${var_worker_role_name:-worker}
 
-for var_k8s_node in $(kubectl get nodes | tail -n +2 | awk '{ print $1 }')
+for var_k8s_node in $(kubectl get nodes | tail -n +2 | grep -i -v 'control-plane' | awk '{ print $1 }')
 do
-	if kubectl get node $var_k8s_node | grep -i 'control-plane' &>/dev/null
-	then
-		continue
-	fi
-
 	kubectl label node $var_k8s_node node-role.kubernetes.io/${var_worker_role_name}=true
 done
