c82996a842e9591461878ca429c4c7d23b3137dd
Muthukumar Subramaniam <msmkumar.eee@gmail.com>
2024-11-14 22:22:31 +0530

    Add: Internal test scripts

--- inst-k8s-ansible/testing/my-local-k8s-setup.py
+++ inst-k8s-ansible/testing/my-local-k8s-setup.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
+import os
+
 # Define the file paths
-host_control_plane_path = './inst-k8s-ansible/host-control-plane'
-host_workers_path = './inst-k8s-ansible/host-workers'
-pod_network_cidr_path = './inst-k8s-ansible/pod-network-cidr'
+host_control_plane_path = '../host-control-plane'
+host_workers_path = '../host-workers'
+pod_network_cidr_path = '../pod-network-cidr'
 
 # Write to host-control-plane
 with open(host_control_plane_path, 'w') as file:
@@ -18,6 +20,8 @@ with open(host_workers_path, 'w') as file:
 with open(pod_network_cidr_path, 'w') as file:
     file.write("10.8.0.0/16\n")
 
-print("Local setup for k8s cluster updated successfully.\nNow you can run setup.py under inst-k8s-ansible directory")
+print("Local setup for k8s cluster installation updated successfully.\nNow you can go back to inst-k8s-ansible directory and run setup.py!")
+
+os.chdir("..")
 
 exit
--- inst-k8s-ansible/testing/my-local-k8s-test-setup.py
+++ inst-k8s-ansible/testing/my-local-k8s-test-setup.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 # Define the file paths
-host_control_plane_path = './inst-k8s-ansible/host-control-plane'
-host_workers_path = './inst-k8s-ansible/host-workers'
-pod_network_cidr_path = './inst-k8s-ansible/pod-network-cidr'
+host_control_plane_path = '../host-control-plane'
+host_workers_path = '../host-workers'
+pod_network_cidr_path = '../pod-network-cidr'
 
 # Write to host-control-plane
 with open(host_control_plane_path, 'w') as file:
@@ -18,6 +18,6 @@ with open(host_workers_path, 'w') as file:
 with open(pod_network_cidr_path, 'w') as file:
     file.write("10.8.0.0/16\n")
 
-print("Local setup for k8s cluster updated successfully.\nNow you can run setup.py under inst-k8s-ansible directory")
+print("Local setup for k8s cluster installation updated successfully.\nNow you can go back to inst-k8s-ansible directory and run setup.py!")
 
 exit
